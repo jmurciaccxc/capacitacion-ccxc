310993ea1e34bd475eaf9693667ee683ca6fe6ad
jmurciaccxc <jmurcia@ccxc.us>
2023-03-29 23:46:38 -0500

    finish talwind

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,10 +1,106 @@
 module.exports = {
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        xs: { max: "768px" },
+      },
       backgroundImage: (theme) => ({
-        "body-pattern": "url('../img/pattern.jpg')",
+        "body-pattern": "url('../img/pattern.png')",
+        banner: "url('../img/backbaner.png')",
       }),
+      backgroundPosition: {
+        "banner-position": "-200px -200px",
+      },
+      colors: {
+        "azul-claro": "#37bcf9",
+        "azul-oscuro": "#0370b9",
+      },
+      animation: {
+        "spin-low": "spin 2s linear infinite",
+        "from-bellow": "fromBellow 500ms linear",
+        "from-right": "fromRigth 300ms linear",
+        "bg-banner": "backBanner 10s linear",
+        "text-banner": "showBannerText 10s linear",
+        "show-card-icon": "showCardIcon 300ms linear",
+        "show-card-category": "showCardCategory 400ms linear",
+        "show-card-desc": "showCardDes 400ms linear",
+      },
+      keyframes: {
+        fromBellow: {
+          "0%": { transform: "translateY(0%)" },
+          "50%": { transform: "translateY(200%)" },
+          "100%": { transform: "translateY(0%)" },
+        },
+        fromRigth: {
+          "0%": { transform: "traslateY(200%)" },
+          "100%": { transform: "translateY(0%)" },
+        },
+        backBanner: {
+          "0%": { "background-position": "0px 0px" },
+          "100%": { "background-position": "-200px -200px" },
+        },
+        showBannerText: {
+          "0%": {
+            transform: "translateX(-600%) scale(5,5)",
+            opacity: 1,
+          },
+          "50%": {
+            transform: "translateX(600%) scale(5,5)",
+            opacity: 1,
+          },
+          "75%": {
+            transform: "translateX(-600%) scale(5,5)",
+            "text-shadow": "none",
+            color: "transparent",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateX(0%) scale(1,1)",
+            "text-shadow": "2px 2px 1px black",
+            color: "white",
+            opacity: 1,
+          },
+        },
+        showCardIcon: {
+          from: {
+            transform: "translateY(-200%)",
+          },
+          to: {
+            transform: "translateY(0%)",
+          },
+        },
+        showCardCategory: {
+          from: {
+            transform: "translateY(-300%)",
+          },
+          to: {
+            transform: "translateY(0%)",
+          },
+        },
+        showCardDes: {
+          from: {
+            transform: "translateX(-300%)",
+          },
+          to: {
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      boxShadow: {
+        header3D:
+          "0px 1px 0px #393d3f,1px 2px 0px #393d3f,2px 3px 0px #393d3f,3px 4px 0px #393d3f",
+      },
     },
-    variants: {},
+    fontFamily: {
+      bebas: ["Bebas Neue"],
+    },
+  },
+  variants: {
+    animation: ["responsive", "hover", "group-hover"],
+    animate: ["responsive", "hover", "group-hover"],
+    fontSize: ["responsive", "hover", "group-hover"],
+    transform: ["responsive", "hover", "group-hover"],
+    scale: ["responsive", "hover", "group-hover"],
   },
 };
