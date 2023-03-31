30e2be7a666583766e8f6c0a1b1ebf87c0cfb2da
jmurciaccxc <jmurcia@ccxc.us>
2023-03-31 09:55:18 -0500

    review-animation

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -33,8 +33,8 @@ module.exports = {
           "100%": { transform: "translateY(0%)" },
         },
         fromRigth: {
-          "0%": { transform: "traslateY(200%)" },
-          "100%": { transform: "translateY(0%)" },
+          "0%": { transform: "traslateX(200%)" },
+          "100%": { transform: "translateX(0%)" },
         },
         backBanner: {
           "0%": { "background-position": "0px 0px" },
@@ -90,10 +90,12 @@ module.exports = {
       boxShadow: {
         header3D:
           "0px 1px 0px #393d3f,1px 2px 0px #393d3f,2px 3px 0px #393d3f,3px 4px 0px #393d3f",
+        box: " 0px 0px 1px rgba(0,0,0,0.3), 0px 3px 7px rgba(0,0,0,0.3), 0px 1px white inset, 0px -3px 1px rgba(0,0,0,0.3) inset",
       },
     },
     fontFamily: {
       bebas: ["Bebas Neue"],
+      sans: ["Open Sans"],
     },
   },
   variants: {
